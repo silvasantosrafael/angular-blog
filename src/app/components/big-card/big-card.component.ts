df3e36d30335f950dc32d7d820a3593320eaa177
silvasantosrafael <rafael.silva318819@gmail.com>
2023-09-09 02:28:13 -0300

    added responsives stylizations and minor fix

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css', './big-card.responsive.component.css']
 })
 export class BigCardComponent implements OnInit {
 
+  @Input()
+  cardPhotoCover: string = '';
+  cardTitle: string = 'Paint do Windows 11 pode ganhar ferramenta para remover fundo de imagens';
+  cardDescription: string = 'Atualização liberada para Windows Insiders traz recurso que identifica automaticamente assunto da imagem e apaga o resto com apenas um clique';
+  cardLink: string = 'https://tecnoblog.net/noticias/2023/09/08/paint-do-windows-11-pode-ganhar-ferramenta-para-remover-fundo-de-imagens/';
+  cardDate: string = this.setCardDate();
+
   constructor() { }
 
+  setCardDate(): string {
+
+    let date: Date = new Date();
+
+    this.cardDate = date.toLocaleDateString();
+
+    return this.cardDate;
+  }
+
   ngOnInit(): void {
+    console.log(this.cardDate)
   }
 
 }
