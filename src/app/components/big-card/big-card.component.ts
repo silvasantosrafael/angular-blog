1a5e1ee41013cbdb3b24e38afbfa0f3cd53a76ab
silvasantosrafael <rafael.silva318819@gmail.com>
2023-09-09 18:20:09 -0300

    added properties in components and minor fix

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -9,24 +9,18 @@ export class BigCardComponent implements OnInit {
 
   @Input()
   cardPhotoCover: string = '';
-  cardTitle: string = 'Paint do Windows 11 pode ganhar ferramenta para remover fundo de imagens';
-  cardDescription: string = 'Atualização liberada para Windows Insiders traz recurso que identifica automaticamente assunto da imagem e apaga o resto com apenas um clique';
-  cardLink: string = 'https://tecnoblog.net/noticias/2023/09/08/paint-do-windows-11-pode-ganhar-ferramenta-para-remover-fundo-de-imagens/';
-  cardDate: string = this.setCardDate();
-
+  @Input()
+  cardTitle: string = '';
+  @Input()
+  cardDescription: string = '';
+  @Input()
+  cardLink: string = '';
+  @Input()
+  cardDate: string = '';
+  
   constructor() { }
 
-  setCardDate(): string {
-
-    let date: Date = new Date();
-
-    this.cardDate = date.toLocaleDateString();
-
-    return this.cardDate;
-  }
-
   ngOnInit(): void {
-    console.log(this.cardDate)
   }
 
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css', './small-card.responsive.component.css']
 })
 export class SmallCardComponent implements OnInit {
+
+  @Input()
+  cardPhotoCover: string = '';
+  @Input()
+  cardTitle: string = '';
+  @Input()
+  cardLink: string = '';
+  @Input()
+  cardDate: string = '';
 
   constructor() { }
 
